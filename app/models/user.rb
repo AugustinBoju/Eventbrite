7c2b4ecbde40a4e39855977ec672fcd475811ece
augustinboju <bojuaugustin@gmail.com>
2020-02-18 17:29:32 +0100

    make it better

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   after_create :welcome_send
 
   has_many :attendances
-  has_many events:, through: :attendances
+  has_many :events, through: :attendances
 
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
